4a4c89a136b4df345c34c282a17115d47eb72da1
Derek <derek@winacta.com>
2025-09-10 17:14:56 -0700

    Update countdown to 11.10.2025

---
+++ src/index.js
@@ -0,0 +1,10 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import QlearScape from "./QlearScape";
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
+  <React.StrictMode>
+    <QlearScape />
+  </React.StrictMode>
+);
